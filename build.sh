d06a0ec64704cacf5c6d3233a4f0fd14c2ca62c8
liljekvist <mikael0225@outlook.com>
2021-12-16 09:46:16 +0100

    made bash build script for selfcontained executable

---
+++ build.sh
@@ -0,0 +1,13 @@
+rm build/*
+
+
+dotnet publish desktop-app/ -r win10-x64 -p:PublishSingleFile=true --self-contained -c Release
+dotnet publish desktop-app/ -r linux-x64 -p:PublishSingleFile=true --self-contained -c Release
+dotnet publish desktop-app/ -r osx-x64 -p:PublishSingleFile=true --self-contained -c Release
+
+mkdir build
+cp /home/liljekvist/Projects/find-chargers-desktop/desktop-app/bin/Release/net6.0/win10-x64/publish/find-chargers-desktop.exe build/find-chargers-windows.exe
+cp /home/liljekvist/Projects/find-chargers-desktop/desktop-app/bin/Release/net6.0/linux-x64/publish/find-chargers-desktop build/find-chargers-linux
+cp /home/liljekvist/Projects/find-chargers-desktop/desktop-app/bin/Release/net6.0/osx-x64/publish/find-chargers-desktop build/find-chargers-osx
+
+echo "Done Files are in build/"

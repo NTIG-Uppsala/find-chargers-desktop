afdb51aabbb88a8e8184109b7c32ad6b62c2753a
liljekvist <mikael0225@outlook.com>
2021-12-16 10:02:27 +0100

    replaced / with \

--- build.bat
+++ build.bat
@@ -1,13 +1,12 @@
 del build\*
 
-
-dotnet publish desktop-app/ -r win10-x64 -p:PublishSingleFile=true --self-contained -c Release
-dotnet publish desktop-app/ -r linux-x64 -p:PublishSingleFile=true --self-contained -c Release
-dotnet publish desktop-app/ -r osx-x64 -p:PublishSingleFile=true --self-contained -c Release
+dotnet publish desktop-app\ -r win10-x64 -p:PublishSingleFile=true --self-contained -c Release
+dotnet publish desktop-app\ -r linux-x64 -p:PublishSingleFile=true --self-contained -c Release
+dotnet publish desktop-app\ -r osx-x64 -p:PublishSingleFile=true --self-contained -c Release
 
 mkdir build
-copy find-chargers-desktop/desktop-app/bin/Release/net6.0/win10-x64/publish/find-chargers-desktop.exe build/find-chargers-windows.exe
-copy find-chargers-desktop/desktop-app/bin/Release/net6.0/linux-x64/publish/find-chargers-desktop build/find-chargers-linux
-copy find-chargers-desktop/desktop-app/bin/Release/net6.0/osx-x64/publish/find-chargers-desktop build/find-chargers-osx
+copy find-chargers-desktop\desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop.exe build/find-chargers-windows.exe
+copy find-chargers-desktop\desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build/find-chargers-linux
+copy find-chargers-desktop\desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build/find-chargers-osx
 
-echo "Done Files are in build/"
+echo "Done Files are in build\"

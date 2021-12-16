cb73ff2d0faa87071353bccd1e00a2a2dee11146
liljekvist <mikael0225@outlook.com>
2021-12-16 10:06:21 +0100

    changed path

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ dotnet publish desktop-app\ -r linux-x64 -p:PublishSingleFile=true --self-contai
 dotnet publish desktop-app\ -r osx-x64 -p:PublishSingleFile=true --self-contained -c Release
 
 mkdir build
-copy find-chargers-desktop\desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop.exe build/find-chargers-windows.exe
-copy find-chargers-desktop\desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build/find-chargers-linux
-copy find-chargers-desktop\desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build/find-chargers-osx
+copy desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop.exe build/find-chargers-windows.exe
+copy desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build/find-chargers-linux
+copy desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build/find-chargers-osx
 
 echo "Done Files are in build\"

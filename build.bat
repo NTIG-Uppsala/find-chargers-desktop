425ec9d0f87463ee5e87a23387b2b8f9747a3b31
liljekvist <mikael0225@outlook.com>
2021-12-16 10:11:31 +0100

    Final touches

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ dotnet publish desktop-app\ -r linux-x64 -p:PublishSingleFile=true --self-contai
 dotnet publish desktop-app\ -r osx-x64 -p:PublishSingleFile=true --self-contained -c Release
 
 mkdir build
-copy desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop.exe build\find-chargers-windows.exe
-copy desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build\find-chargers-linux
+copy desktop-app\bin\Release\net6.0\win10-x64\publish\find-chargers-desktop.exe build\find-chargers-windows.exe
+copy desktop-app\bin\Release\net6.0\linux-x64\publish\find-chargers-desktop build\find-chargers-linux
 copy desktop-app\bin\Release\net6.0\osx-x64\publish\find-chargers-desktop build\find-chargers-osx
 
 echo "Done Files are in build\"
